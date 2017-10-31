d7373f83de54263b32acf211b0e64d61a1841e8f
juliodelacruzz <julio.dc@icloud.com>
2017-10-31 11:53:59 -0700

    10/31/17

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -42,17 +42,17 @@ public class Calculate {
 		return (wholeNum + "_" + Numerator + "/" + b);
 
 	}
-	//Part Two
 	//This method converts a binomial multiplication 
 	public static String foil (int a, int b, int c, int d) {
 		int first = (a*d);
 		int second = (a*d) + (b*c);
 		int third = (b*d);
 		return (first + "n^2" + "+" + second + "n" + "+" + third);
-
+	
 	}
+	//Part Two
 	//This method determines whether or not one integer is evenly divisible by another. It accepts two integers and returns a boolean
-	public static boolean isDivisbleBy (int a, int b) {
+	public static boolean isDivisibleBy (int a, int b) {
 		if (a % b == 0) {
 			return (true);
 		}	else {
